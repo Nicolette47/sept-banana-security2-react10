c42889bc22fe759d7ea3ecde369e6dd958b21081
nvanzeijl <nvanzeijl@gmail.com>
2025-02-03 21:15:04 +0100

    login en log uit met token klaar

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,7 @@
 import React, {useState, createContext} from 'react';
 import {useNavigate} from "react-router-dom";
+import {jwtDecode} from "jwt-decode";
+import axios from 'axios';
 
 
 export const AuthContext = createContext({});
@@ -10,22 +12,62 @@ function AuthContextProvider({children}) {
     const [isAuth, setIsAuth] = useState({
         isAuth: false,
         user: null,
+        status: 'pending',
     });
     const navigate = useNavigate();
 
 
-    function login(token) {
-        localStorage.setItem('token', token);
+    async function login(token) {
         console.log("De gebruiker is ingelogd");
-        setIsAuth(true);
+
+        localStorage.setItem('token', token);
+
+        const decodedToken = jwtDecode(token)
+        console.log(decodedToken.sub);
+
+    try {
+        const result = await axios.get(`http://localhost:3000/600/users/${decodedToken.sub}`, {
+            headers:{
+            "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            }})
+        console.log(result);
+        setIsAuth({
+            isAuth: true,
+            user: {
+                username:result.data.username,
+                email: result.data.email,
+                id: result.data.id,
+            }
+        })
+
+    } catch (e) {
+        console.error(e);
+        localStorage.removeItem('token');
+        setIsAuth ({
+            isAuth: false,
+            user: null,
+        })
+
+    }
         navigate('/profile');
     }
 
+
+
     function logout() {
         console.log("De gebruiker is uitgelogd");
-        setIsAuth(false);
+
+        localStorage.removeItem('token');
+
+        setIsAuth ({
+            isAuth: false,
+            user: null,
+        })
+
         navigate('/');
     }
+
 
     const data = {
         isAuth: isAuth.isAuth,
